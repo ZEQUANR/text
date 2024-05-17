56fba974a2a627dcf3691cbe4287179996a235e2
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-05-17 12:03:00 +0800

    c

--- cmd/test/test.go
+++ cmd/test/test.go
@@ -1,14 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
 
-	const len = 10
-	var a = [len + 10]int{1, 2, 4}
+	// // 全局：
+	// var arr0 [5]int = [5]int{1, 2, 3}
+	// var arr1 = [5]int{1, 2, 3, 4, 5}
+	// var arr2 = [...]int{1, 2, 3, 4, 5, 6}
+	// var str = [5]string{3: "hello world", 4: "tom"}
+	// // 局部：
+	// a := [3]int{1, 2}           // 未初始化元素值为 0。
+	// b := [...]int{1, 2, 3, 4}   // 通过初始化值确定数组长度。
+	// c := [5]int{2: 100, 4: 200} // 使用索引号初始化元素。
+	// d := [...]struct {
+	// 	name string
+	// 	age  uint8
+	// }{
+	// 	{"user1", 10}, // 可省略元素类型。
+	// 	{"user2", 20}, // 别忘了最后一行的逗号。
+	// }
 
-	fmt.Println(reflect.TypeOf(a))
+	var arr2 = []int{1, 2, 3, 4, 5, 6}
+
+	fmt.Println(cap(arr2), len(arr2))
 }
